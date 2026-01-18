f1f617ddf5302b6cd5052510a830d87caed41fd4
Mark Lozano <mark.d.lozano@gmail.com>
2026-01-17 22:21:29 -0700

    fix: make post-login page dynamic to prevent build-time prerender

--- src/app/(app)/post-login/page.tsx
+++ src/app/(app)/post-login/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import { hasDigests } from "@/lib/db";
 
+export const dynamic = "force-dynamic";
+
 export default async function PostLoginPage() {
   const digestsExist = await hasDigests();
 
